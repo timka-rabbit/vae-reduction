Fix normalization

src/core/data_handling/normalization/normalizer_class.py
@@ -3,6 +3,8 @@ import numpy as np
 from core.data_description import DataDescription
 from core.data_class import Data
 
+from multipledispatch import dispatch
+
 
 class Normalizer(object):
     """
@@ -10,13 +12,12 @@ class Normalizer(object):
     """
 
     @staticmethod
-    def norm(data, norm_min: float = 0., norm_max: float = 1.) -> Data:
+    @dispatch(Data)
+    def norm(data: Data) -> Data:
         """
         Нормировка данных
 
         :param data: Data. Данные для нормировки.
-        :param norm_min: float. Нижняя граница нормированных данных (по умолчанию 0).
-        :param norm_max: float. Верхняя граница нормированных данных (по умолчанию 1).
         :return: Data. Нормированные данные.
         """
         x_dim = data.description.x_dim
@@ -25,18 +26,36 @@ class Normalizer(object):
         delta_x = np.array([bounds_x_1[i] - bounds_x_0[i] for i in range(x_dim)])
         bounds_y_0, bounds_y_1 = zip(*data.description.y_bounds)
         delta_y = np.array([bounds_y_1[i] - bounds_y_0[i] for i in range(y_dim)])
-        delta_norm = norm_max - norm_min
 
-        norm_x = norm_min + ((data.x.copy() - np.array(bounds_x_0)) * delta_norm / delta_x)
-        norm_y = norm_min + ((data.y.copy() - np.array(bounds_y_0)) * delta_norm / delta_y)
+        norm_x = (data.x.copy() - np.array(bounds_x_0)) / delta_x
+        norm_y = (data.y.copy() - np.array(bounds_y_0)) / delta_y
         return Data(x=norm_x, y=norm_y,
                     description=DataDescription(x_dim=data.description.x_dim,
                                                 y_dim=data.description.y_dim,
-                                                x_bounds=[(norm_min, norm_max) for i in range(x_dim)],
-                                                y_bounds=[(norm_min, norm_max) for i in range(y_dim)]))
+                                                x_bounds=[(0, 1) for i in range(x_dim)],
+                                                y_bounds=[(0, 1) for i in range(y_dim)]))
 
     @staticmethod
-    def denorm(data, x_bounds, y_bounds) -> Data:
+    @dispatch(object, list)
+    def norm(x: np.ndarray, x_bounds) -> np.ndarray:
+        """
+        Нормировка массива точек
+
+        :param x: np.ndarray. Массив точек для нормировки.
+        :param x_bounds: List[Tuple[float, float]]. Границы данных.
+        :return: np.ndarray. Нормированный массив точек.
+        """
+        assert x.shape[1] == len(x_bounds)
+        x_dim = len(x_bounds)
+        bounds_x_0, bounds_x_1 = zip(*x_bounds)
+        delta_x = np.array([bounds_x_1[i] - bounds_x_0[i] for i in range(x_dim)])
+
+        norm_x = ((x.copy() - np.array(bounds_x_0)) / delta_x)
+        return norm_x
+
+    @staticmethod
+    @dispatch(Data, list, list)
+    def denorm(data: Data, x_bounds, y_bounds) -> Data:
         """
         Денормировка данных
 
@@ -64,3 +83,21 @@ class Normalizer(object):
                                                 y_dim=data.description.y_dim,
                                                 x_bounds=x_bounds,
                                                 y_bounds=y_bounds))
+
+    @staticmethod
+    @dispatch(object, list)
+    def denorm(x: np.ndarray, x_bounds) -> Data:
+        """
+        Денормировка масиива точек
+
+        :param x: np.ndarray. Массив точек для денормировки.
+        :param x_bounds: List[Tuple[float, float]]. Границы денормированных данных.
+        :return: np.ndarray. Нормированный массив точек.
+        """
+        assert x.shape[1] == len(x_bounds)
+        x_dim = len(x_bounds)
+        bounds_x_0, bounds_x_1 = zip(*x_bounds)
+        delta_x = np.array([bounds_x_1[i] - bounds_x_0[i] for i in range(x_dim)])
+
+        x = np.array(bounds_x_0) + (x.copy() * delta_x)
+        return x

src/test/data_handling/normalization/test_normalization.py
@@ -10,22 +10,40 @@ from unittest import TestCase
 
 
 class TestNormalization(TestCase):
-    def test_ellipse_norm(self):
+    def test_ellipse_norm_1(self):
         func = Ellipse(rx=4, ry=2)
         x = Grid().get_data(func.description, 100)
         y = func.evaluate(x)
         data = Data(y[:, 0, np.newaxis], y[:, 1, np.newaxis])
         cu.plot2d(data.x, data.y, 'Обычный график')
-
         norm_data = Normalizer.norm(data)
         cu.plot2d(norm_data.x, norm_data.y, 'Нормированный график')
 
-    def test_ellipse_denorm(self):
+    def test_ellipse_norm_2(self):
+        func = Ellipse(rx=4, ry=2)
+        x = Grid().get_data(func.description, 100)
+        y = func.evaluate(x)
+        data = Data(y[:, 0, np.newaxis], y[:, 1, np.newaxis])
+        cu.plot2d(data.x, data.y, 'Обычный график')
+        norm_x = Normalizer.norm(data.x, x_bounds=[(-4, 4)])
+        norm_y = Normalizer.norm(data.y, x_bounds=[(-2, 2)])
+        cu.plot2d(norm_x, norm_y, 'Нормированный график')
+
+    def test_ellipse_denorm_1(self):
         func = Ellipse(center=(0.5, 0.5), rx=0.5, ry=0.5)
         x = Grid().get_data(func.description, 100)
         y = func.evaluate(x)
         data = Data(y[:, 0, np.newaxis], y[:, 1, np.newaxis])
         cu.plot2d(data.x, data.y, 'Обычный график')
+        denorm_data = Normalizer.denorm(data, x_bounds=[(-4, 4)], y_bounds=[(-2, 2)])
+        cu.plot2d(denorm_data.x, denorm_data.y, 'Денормированный график')
 
-        norm_data = Normalizer.denorm(data, x_bounds=[(-4, 4)], y_bounds=[(-2, 2)])
-        cu.plot2d(norm_data.x, norm_data.y, 'Денормированный график')
+    def test_ellipse_denorm_2(self):
+        func = Ellipse(center=(0.5, 0.5), rx=0.5, ry=0.5)
+        x = Grid().get_data(func.description, 100)
+        y = func.evaluate(x)
+        data = Data(y[:, 0, np.newaxis], y[:, 1, np.newaxis])
+        cu.plot2d(data.x, data.y, 'Обычный график')
+        denorm_x = Normalizer.denorm(data.x, x_bounds=[(-4, 4)])
+        denorm_y = Normalizer.denorm(data.y, x_bounds=[(-2, 2)])
+        cu.plot2d(denorm_x, denorm_y, 'Денормированный график')