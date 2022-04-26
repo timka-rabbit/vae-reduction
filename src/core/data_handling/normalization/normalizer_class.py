220d27e7c1d52168071acad227747a87a320fdd1
Artyom <ar.buyanow@yandex.ru>
2022-04-27 01:37:00 +0300

    Added verify to normalization

--- src/core/data_handling/normalization/normalizer_class.py
+++ src/core/data_handling/normalization/normalizer_class.py
@@ -7,13 +7,7 @@ class Normalizer(object):
     """
 
     @staticmethod
-    def norm(x: np.ndarray, x_bounds) -> np.ndarray:
-        """
-        Нормировка массива точек
-        :param x: np.ndarray. Массив точек для нормировки.
-        :param x_bounds: List[Tuple[float, float]]. Границы данных.
-        :return: np.ndarray. Нормированный массив точек.
-        """
+    def _verify(x: np.ndarray, x_bounds):
         assert x.shape[1] >= len(x_bounds)
         if x.shape[1] > len(x_bounds):
             x_bounds.extend([(0, 1)]*np.abs(x.shape[1] - len(x_bounds)))
@@ -21,7 +15,19 @@ class Normalizer(object):
         bounds_x_0, bounds_x_1 = zip(*x_bounds)
         delta_x = np.array([bounds_x_1[i] - bounds_x_0[i] for i in range(x_dim)]).reshape(1, x_dim)
 
-        norm_x = ((x - np.array(bounds_x_0).reshape(1, x_dim)) / delta_x)
+        return np.array(bounds_x_0).reshape(1, x_dim), delta_x
+
+    @staticmethod
+    def norm(x: np.ndarray, x_bounds) -> np.ndarray:
+        """
+        Нормировка массива точек
+        :param x: np.ndarray. Массив точек для нормировки.
+        :param x_bounds: List[Tuple[float, float]]. Границы данных.
+        :return: np.ndarray. Нормированный массив точек.
+        """
+        bounds_0, delta_x = Normalizer._verify(x, x_bounds)
+
+        norm_x = ((x - bounds_0) / delta_x)
         return norm_x
 
     @staticmethod
@@ -32,12 +38,7 @@ class Normalizer(object):
         :param x_bounds: List[Tuple[float, float]]. Границы денормированных данных.
         :return: np.ndarray. Нормированный массив точек.
         """
-        assert x.shape[1] >= len(x_bounds)
-        if x.shape[1] > len(x_bounds):
-            x_bounds.extend([(0, 1)]*np.abs(x.shape[1] - len(x_bounds)))
-        x_dim = len(x_bounds)
-        bounds_x_0, bounds_x_1 = zip(*x_bounds)
-        delta_x = np.array([bounds_x_1[i] - bounds_x_0[i] for i in range(x_dim)]).reshape(1, x_dim)
+        bounds_0, delta_x = Normalizer._verify(x, x_bounds)
 
-        x = np.array(bounds_x_0).reshape(1, x_dim) + (x * delta_x)
+        x = bounds_0 + (x * delta_x)
         return x
