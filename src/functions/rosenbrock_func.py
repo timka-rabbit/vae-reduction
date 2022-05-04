a8c00a77b5a367339d38a8c4f5a21b715c9fb283
Artyom <ar.buyanow@yandex.ru>
2022-05-04 11:36:04 +0300

    Added generalized Rosenbrock

--- src/functions/rosenbrock_func.py
+++ src/functions/rosenbrock_func.py
@@ -6,16 +6,24 @@ from functions.abstract_function import AbstractFunc
 
 class Rosenbrock(AbstractFunc):
     """
-    Функция Розенброка
+    Функция Розенброка размерности n
     """
 
-    def __init__(self):
-        super().__init__(DataDescription(x_dim=2, y_dim=1, x_bounds=[(-2, 2), (-2, 2)]))
+    def __init__(self, n=2):
+        """
+        :param n: int. Размерность функции.
+        """
+        super().__init__(DataDescription(x_dim=n, y_dim=1, x_bounds=[(-2, 2)]*n))
+        self.n = n
 
     @property
     def name(self) -> str:
-        return "Функция Розенброка"
+        return "Функция Розенброка размерности n"
 
     def evaluate(self, points) -> np.ndarray:
         points = self._verify(points)
-        return ((1 - points[:, 0]) ** 2 + 100 * (points[:, 1] - points[:, 0] ** 2) ** 2).reshape(-1, 1)
+        y = np.zeros((points.shape[0],))
+        for j in range(points.shape[0]):
+            for i in range(self.n - 1):
+                y[j] += (1 - points[j, i]) ** 2 + 100 * (points[j, i + 1] - points[j, i] ** 2) ** 2
+        return y.reshape(-1, 1)
