b52efa5c4c5386775e822f2e450511b3e04f7a9f
Artyom <ar.buyanow@yandex.ru>
2022-05-15 22:23:34 +0300

    Add search tests

--- src/test/nets/test_vae.py
+++ src/test/nets/test_vae.py
@@ -1,5 +1,6 @@
 from core.optimazation.ego import EGO
 from core.optimazation.minimize import Minimizer
+from core.optimazation.brute_force import BruteForce
 from functions.abstract_function import AbstractFunc
 from core.data_handling.generators.lhs_sequence import LHS
 from core.data_handling.generators.sobol_sequence import Sobol
@@ -10,6 +11,7 @@ from core.metrics.MAE import MAE
 from core.nets.vae import VAE
 
 from functions.rastrigin_func import Rastrigin
+from functions.rosenbrock_func import Rosenbrock
 
 import numpy as np
 from unittest import TestCase
@@ -49,12 +51,54 @@ class TestVAE(TestCase):
 
     # ---------------------------------------------------------------------------------------------------------------- #
 
+    def test_vae_search(self, func: AbstractFunc, opt: float, irr_dim: int = 0):
+        batch_size = 50  # размер батчей
+        fit_count = 400  # размерность выборки для обучения
+        x_dim = func.description.x_dim  # размерность пространства функции
+        dim = x_dim + irr_dim  # полная размерность X
+        h_dim = 3
+
+        x = Sobol().get_data(description=func.description, samples_num=fit_count, irrelevant_var_count=irr_dim)
+        y = func.evaluate(x)
+        data = Data(x, y, DataDescription(dim, func.description.y_dim))
+        vae = VAE(description=data.description,
+                  enc_size=[5], dec_size=[5], epochs=20,
+                  batch_size=batch_size, hidden_dim=h_dim)
+        vae.fit(data)
+        vae.save(f'../../../data/net weights/vae/{func.name}_{dim}_{h_dim}.h5')
+        #vae.load(f'../../../data/net weights/vae/{func.name}_{dim}_{h_dim}.h5')
+
+        def opt_f(x):
+            pred = vae.predict_decoder(np.array(x).reshape((1, h_dim)))[0]
+            pred_x, pred_y = np.split(pred, indices_or_sections=[dim], axis=0)
+            return pred_y[0]
+
+        h_desc = DataDescription(h_dim, 1, x_bounds=[(0, 1) for i in range(h_dim)])
+        res = BruteForce(h_desc, 10).optimize(func=opt_f)
+
+        pred = vae.predict_decoder(np.array(res).reshape((1, h_dim)))[0]
+        opt_x, opt_y = np.split(pred, indices_or_sections=[dim], axis=0)
+        print(f'{func.name}')
+        print(f'Min pred Y: {opt_y[0]}')
+        print(f'F(pred X): {func.evaluate(opt_x).ravel()[0]}')
+        print(f'Min real Y: {opt}')
+
+    def test_vae_search_rastrigin(self):
+        func = Rastrigin(n=6)
+        self.test_vae_search(func=func, opt=0, irr_dim=4)
+
+    def test_vae_search_rosenbrock(self):
+        func = Rosenbrock(n=6)
+        self.test_vae_search(func=func, opt=0, irr_dim=4)
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+
     def test_vae_ego_parameters(self, func: AbstractFunc, irr_dim: int = 0):
         n_iter = 10  # количество итераций EGO
         batch_size = 25  # размер батчей
         criterion = 'EI'  # критерий выбора следующей точки EGO
         x_dim = func.description.x_dim  # размерность пространства функции
-        fit_count = 10000  # размерность выборки для обучения
+        fit_count = 5000  # размерность выборки для обучения
         test_count = 200  # размерность выборки для тестирования
         dim = x_dim + irr_dim  # полная размерность X
 
@@ -64,6 +108,7 @@ class TestVAE(TestCase):
 
         test_x = LHS().get_data(description=func.description, samples_num=test_count, irrelevant_var_count=irr_dim)
         test_y = func.evaluate(test_x)
+        test = np.hstack((test_x, test_y))
 
         def predict_params(point: np.ndarray) -> np.ndarray:
             """
@@ -76,40 +121,40 @@ class TestVAE(TestCase):
             res = np.zeros((point_count, 1))
 
             for i in range(point_count):
-                n_epoch = int(point[i, 0])
-                h_dim = int(point[i, 1])
+                n_epoch = round(point[i, 0])
+                h_dim = round(point[i, 1])
 
                 vae = VAE(description=data.description,
                           enc_size=[4], dec_size=[4], epochs=n_epoch,
                           batch_size=batch_size, hidden_dim=h_dim)
                 vae.fit(data)
                 vae.save(f'../../../data/net weights/vae/{func.name}_ego_{dim}_{h_dim}.h5')
-                for j in range(vae.batch_size, test_count, vae.batch_size):
-                    pred_x = vae.predict(test_x[j-batch_size:j, :])
-                    pred_y = func.evaluate(pred_x)
-                    res[i] += MAE().evaluate(test_y[j-batch_size:j, :], pred_y)
+                for i in range(0, test_count, batch_size):
+                    pred = vae.predict(test[i:i + batch_size, :])
+                    pred_x, pred_y = np.split(pred, indices_or_sections=[dim], axis=1)
+                    res[i] += MAE().evaluate(test_y[i:i + batch_size, :], pred_y)
                 res[i] /= (test_count / batch_size)
             return res
 
         # устанавливаем границы: epoch, batch_size, hidden_dim
-        params = DataDescription(x_dim=3, y_dim=1, x_bounds=[(10, 40), (1, 3)])
+        params = DataDescription(x_dim=2, y_dim=1, x_bounds=[(10, 40), (1, 3)])
         ego = EGO(data_description=params, criterion=criterion)
         opt_params = ego.optimize(func=predict_params, n_iter=n_iter)
 
         epochs, hidd = opt_params.squeeze()
-        print(f'\nEpochs: {int(epochs)}\nHidden dim: {int(hidd)}')
-        vae = VAE(func=func, enc_size=[4], dec_size=[4], epochs=int(epochs),
-                  batch_size=batch_size, hidden_dim=int(hidd))
-        vae.load(f'../../../data/net weights/vae/{func.name}_ego_{x_dim+irr_dim}_{int(hidd)}.h5')
+        print(f'\nEpochs: {round(epochs)}\nHidden dim: {round(hidd)}')
+        vae = VAE(func=func, enc_size=[4], dec_size=[4], epochs=round(epochs),
+                  batch_size=batch_size, hidden_dim=round(hidd))
+        vae.load(f'../../../data/net weights/vae/{func.name}_ego_{x_dim+irr_dim}_{round(hidd)}.h5')
 
-        err = 0
-        for j in range(vae.batch_size, test_count, vae.batch_size):
-            pred_x = vae.predict(test_x[j - batch_size:j, :])
-            pred_y = func.evaluate(pred_x)
-            err += MAE().evaluate(test_y[j - batch_size:j, :], pred_y)
-        err /= (test_count / batch_size)
-        print(f'Error: {err}')
+        res = 0
+        for i in range(0, test_count, batch_size):
+            pred = vae.predict(test[i:i + batch_size, :])
+            pred_x, pred_y = np.split(pred, indices_or_sections=[dim], axis=1)
+            res += MAE().evaluate(test_y[i:i + batch_size, :], pred_y)
+        res /= (test_count / batch_size)
+        print(f'Error: {res}')
 
     def test_vae_ego_rastrigin(self):
         func = Rastrigin(n=6)
-        self.test_vae_ego_parameters(func=func, irr_dim=0)
+        self.test_vae_ego_parameters(func=func, irr_dim=2)
